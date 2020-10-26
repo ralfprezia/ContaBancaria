f80da8d653a4618e7f7eea4ee9d88f6f18bfbdb8
Ralf Prezia <62730168+ralfprezia@users.noreply.github.com>
2020-10-25 23:26:06 -0300

    Add files via upload

---
+++ ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace ContaBancaria {
+    class ContaBancaria {
+        //atributos
+        public int Numero { get; private set; }//encapsulamento sem alteração somente acesso
+        public string Titular { get; set; }// pode ser alterado o nome do titular
+        public double Saldo { get; private set; }//somente será alterado por um método interno
+
+
+        //Construtor com dois argumentos
+        public ContaBancaria(int numero, string titular) {//quando for criado um objeto conta bancária o saldo recebe = 0.00
+            Numero = numero;
+            Titular = titular;
+        }
+        //Sobrecarga neste construtor com 3 argumentos com o de cima, utilizando o :This(..., ....)
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular) {
+            Deposito(depositoInicial);
+        }
+        //implementando a operação Depósito com void para alterar o valor dela
+        public void Deposito(double quantia) {
+            Saldo += quantia;
+        }
+
+        //implementando a operação Saque com void para alterar o valor dela
+        public void Saque(double quantia) {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString() {//para a exibição do resultado em forma de string concatenado e configurado para ser exibido em um formato assim: Conta 7801, Tiular: Maria Brown, Saldo: $0.00
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);// CultureInfo.InvariantCulture faz com que a virgula seja trocada por um ponto 
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria {
+    class Program {
+        static void Main(string[] args) {
+
+            ContaBancaria conta;// declarando uma conta
+            
+            //Entrada de dados nas variáveis temporárias
+            Console.WriteLine("Entre como o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Haverá deposito inicial (s/n)?");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S') {
+                Console.WriteLine("Entre com o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);//cria a conta com o depósito inicial//chamada  do método construtor com 3 argumentos
+            }
+            else {
+                conta = new ContaBancaria(numero, titular);//cria a conta sem o depósito inicial//chamada do método construtor com 2 argumentos e este contrutor joga o valor do saldo pra 0.00 por não ter depoísto inicial
+            }
+            //interação 
+            Console.WriteLine();//quebra delinha
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Entre com um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);//mostra a conta 
+
+            Console.WriteLine();//quebra de linha
+            Console.WriteLine("Entre com o valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);//ENTRADA DE DADOS
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);    
+        }
+    }
+}
